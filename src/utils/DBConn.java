0ab7b51fd940917062f9deb8dfc3a9fe282daad2
Matthew Shelbourn <matthew@shelbourn.io>
2020-09-29 15:10:26 -0700

    Build DB util class for connecting to MySQL DB. :card_file_box:

--- src/utils/DBConn.java
+++ src/utils/DBConn.java
@@ -3,6 +3,10 @@
  */
 package utils;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Matthew Shelbourn <mshelbo@wgu.edu>
@@ -10,11 +14,33 @@ package utils;
 public class DBConn {
 
     // JDBC URL Components
-    private static final String protocol = "jdbc";
-    private static final String vendor = ":mysql:";
-    private static final String ipAddress = "//3.227.166.251/U06BSb";
+    private static final String PROTOCOL = "jdbc";
+    private static final String VENDOR = ":mysql:";
+    private static final String IPADDRESS = "//3.227.166.251/U06BSb";
 
     // Concatenated JDBC URL
-    private static final String jdbcURL = protocol + vendor + ipAddress;
+    private static final String JDBCURL = PROTOCOL + VENDOR + IPADDRESS;
+
+    // Reference to MySQL JDBC Driver Interface
+    private static final String MYSQLJDBCDriver = "com.mysql.jdbc.Driver";
+    private static Connection conn = null;
+
+    // MySQL DB Username
+    private static final String username = "U06BSb";
+
+    // MySQL DB Password
+    private static final String password = "53688716173";
+
+    // Method to start connection to MySQL DB
+    public static Connection startConnection() {
+        try {
+            Class.forName(MYSQLJDBCDriver);
+            conn = (Connection) DriverManager.getConnection(JDBCURL, username, password);
+            System.out.print("Connection successful!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 
 }
